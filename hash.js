0ba640611833b2cf187b23551b8d02c93f9515c9
root <root@network.powernet.vn>
2025-09-04 00:39:16 +0700

    Finish BE

--- hash.js
+++ hash.js
@@ -1,3 +1,3 @@
-const phoneNumber = "0357738634";
+const phoneNumber = "0337100373";
 const encoded = Buffer.from(phoneNumber, 'utf8').toString('base64');  // Mã hóa chuỗi số thành base64
 console.log("Encoded phone number:", encoded);  // Ví dụ kết quả: "MzozMzE3NzI4OQ=="
